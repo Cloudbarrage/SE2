3fd9f4d8d0ae4bef9d2f88e95d2a243005a731dc
nkaeming <niklas.kaeming@yahoo.com>
2015-06-11 22:19:07 +0200

    Implementierte Version
    Sollte funktionieren. Implementation ohne annonyme Klassen und Java 8.
    Einige Kommentare müssen noch angepasst werden. Entspricht weitestgehend
    dem erstellten UML Diagramm.

---
+++ Kinoticketverkauf_Implementiert/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/SubWerkzeugObserver.java
@@ -0,0 +1,24 @@
+package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge;
+
+/**
+ * Interface für Beobachter, die sich für Änderungen eines ObservableSubWerkzeug
+ * interessieren.
+ * 
+ * @author SE2-Team
+ * @version SoSe 2015
+ */
+public interface SubWerkzeugObserver
+{
+    /**
+     * Diese Operation wird aufgerufen, sobald sich an an dem beobachteten
+     * Werkzeug etwas relevantes geändert hat.
+     * 
+     * Implementierende Klassen müssen in dieser Operation auf die Aenderung
+     * reagieren.
+     * 
+     * @param w - Das Subwerkzeug welches reagiere auf Änderung aufruft.
+     * 
+     */
+	//TODO: annonyme Klassen
+    void reagiereAufAenderung(ObservableSubWerkzeug w);
+}
