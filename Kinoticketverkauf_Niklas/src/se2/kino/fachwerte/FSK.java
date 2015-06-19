686ae757676e0bf58737d25a1fb6b26d719d8f9c
nkaeming <niklas.kaeming@yahoo.com>
2015-06-19 09:03:49 +0200

    Kinoticketsystem Aufgabenblatt 8
    Die Vorlage in Eclipse importiert und einige Refactorbefehle ausgeführt.

---
+++ Kinoticketverkauf_Niklas/src/se2/kino/fachwerte/FSK.java
@@ -0,0 +1,12 @@
+package se2.kino.fachwerte;
+
+/**
+ * Die Altersfreigaben der FSK.
+ * 
+ * @author SE2-Team
+ * @version April 2010.
+ */
+public enum FSK
+{
+    FSK0, FSK6, FSK12, FSK16, FSK18;
+}
